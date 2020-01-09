b90a96cbd9a31ccb78d74a8b44170c03e2dc2d3f
David Santeli <rnd25david@gmail.com>
2020-01-09 09:08:49 +0000

    Creando el modelo centros hasta Factory

---
+++ database/factories/CentroFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Centro;
+use Faker\Generator as Faker;
+
+$factory->define(Centro::class, function (Faker $faker) {
+    return [
+        'codigo' => $faker->unique()->randomNumber,
+        'nombre' => $faker->company,
+        'web' => $faker->url,
+        'verificado' => $faker->boolean,
+        'remember_token' => Str::random(10),
+    ];
+});
