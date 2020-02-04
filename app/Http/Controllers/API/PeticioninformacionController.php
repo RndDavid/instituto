287d43560f4c14c3568e1b9b9ef9eadfb3454012
Tharduk <tharduk@gmail.com>
2020-02-04 10:11:19 +0100

    Creado hasta PeticioninformacionResource

---
+++ app/Http/Controllers/API/PeticioninformacionController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Peticioninformacion;
+use Illuminate\Http\Request;
+
+class PeticioninformacionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Peticioninformacion  $peticioninformacion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Peticioninformacion $peticioninformacion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Peticioninformacion  $peticioninformacion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Peticioninformacion $peticioninformacion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Peticioninformacion  $peticioninformacion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Peticioninformacion $peticioninformacion)
+    {
+        //
+    }
+}
