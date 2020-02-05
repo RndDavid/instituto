279ad24ffca2a80b829a394e295b25ddc4a5cea6
Tharduk <tharduk@gmail.com>
2020-02-05 14:03:25 +0100

    Peticioninformacion completado

--- app/Http/Resources/PeticioninformacionResource.php
+++ app/Http/Resources/PeticioninformacionResource.php
@@ -14,6 +14,12 @@ class PeticioninformacionResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        //return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'tutorGrupo' => $this->tutorGrupoObject,
+            'alumno' => $this->alumnoObject,
+            'observaciones' => $this->observaciones,
+        ];
     }
 }
